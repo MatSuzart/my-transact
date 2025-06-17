1bcd8f1fa5e2cee9ad7f2cc0b917b6c960cfa49b
MatSuzart <mathsuzart@outlook.com>
2025-06-17 18:58:32 -0300

    cart-empty

---
+++ src/components/cart/cart-empty.tsx
@@ -0,0 +1,13 @@
+"use-client"
+
+import { useCart } from "@/app/stores/cart";
+import { Button } from "../ui/button";
+export const CartEmpty = ()=> {
+    const { setOpen } = useCart();
+    return (
+        <div className="my-10 text-center">
+            <p className="mb-4">Empty</p>
+            <Button onClick={()=>setOpen(false)}>Fechar</Button>
+        </div>
+    );
+}
--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -2,6 +2,7 @@
 import { useCart } from "@/app/stores/cart";
 import { Drawer, DrawerContent, DrawerTitle } from "../ui/drawer";
 import { useEffect, useState } from "react";
+import { CartEmpty } from "./cart-empty";
 
 export const Cart = () =>{
     const cart = useCart();
@@ -19,7 +20,8 @@ export const Cart = () =>{
         >
             <DrawerContent>
                 <DrawerTitle>Carrinho</DrawerTitle>
-                ...
+                {cart.items.length <=0 && <CartEmpty></CartEmpty>}
+
             </DrawerContent>
         </Drawer>    
     );
