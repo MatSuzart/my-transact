4b76bce25660ede3ced8f00489d9ba7c167e50f7
MatSuzart <mathsuzart@outlook.com>
2025-08-02 18:54:42 -0300

    cart-view-list-finished

--- src/components/cart/cart-product.tsx
+++ src/components/cart/cart-product.tsx
@@ -1,12 +1,41 @@
 "use client"
 
+import { useProducts } from "@/app/stores/products";
+import { decimalToMoney } from "@/lib/utils";
 import { CartItem } from "@/types/cart-item";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { useState } from "react";
 
 type Props = {
     data: CartItem;
 }
 export const CartProduct = ({ data }:Props)=>{
+    const [qt,setQt] = useState(data.quantity)
+
+    const products = useProducts();
+    let product = products.stuff.find(item=> item.id === data.productId);
+    if(!product) return null;
     return (
-        <div>..</div>
+        <div className="flex- item-center gap-3">
+            <div className="w-10">
+                <Image 
+                src={product.image}
+                alt={product.name}
+                width={100}
+                height={100}
+                className="w-full"
+                />
+            </div>
+            <div className="flex-1">
+                <div>{product.name}</div>
+                <div className="text-sm">{decimalToMoney(product.price)}</div>
+            </div>
+            <div className="flex items-center bg-secondary p-2 rounded-md">
+                <Button size="sm" variant="ghost">-</Button>
+                <div className="mx-3">{qt}</div>
+                <Button size="sm" variant="ghost">+</Button>
+            </div>
+        </div>
     )
 }
