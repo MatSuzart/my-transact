2894566cf5b788a848a039634321cdc0ede2b749
MatSuzart <mathsuzart@outlook.com>
2025-08-05 18:05:49 -0300

    cart-list-amount

--- src/components/cart/cart-list.tsx
+++ src/components/cart/cart-list.tsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 import { useProducts } from "@/app/stores/products";
 import { useState } from "react";
 import { CartProduct } from "./cart-product";
+import { decimalToMoney } from "@/lib/utils";
 
 export const CartList = () => {
     const cart = useCart();
@@ -11,6 +12,17 @@ export const CartList = () => {
 
     const [subtotal, setSubtotal] = useState(0)
     const [shippingCost, setShippingCost] = useState(0)
+
+    const calculateState = ()=>{
+        let sub = 0;
+        for(let item of cart.items){
+            const product = products.stuff.find(pitem =>pitem.id === item.productId)
+            if(product) sub += item.quantity * parseFloat(product.price.toString());
+
+        }
+        setSubtotal(sub)
+    }
+
     return (
         <>
         <div className="flex flex-col gap-3 my-5">
@@ -21,10 +33,10 @@ export const CartList = () => {
                 />
             ))}
         </div>
-        <div>
-            <div>Sub-Total:</div>
-            <div>Frete:</div>
-            <div>Total:</div>
+        <div className="my-4 text-right">
+            <div>Sub-Total:{decimalToMoney(subtotal)}</div>
+            <div>Frete:{decimalToMoney(shippingCost)}</div>
+            <div className="font-bold">Total:{decimalToMoney(subtotal+shippingCost)}</div>
         </div>
         <Button>Finalizar Compra</Button>
         </>
