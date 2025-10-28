9acfa222d519862af482f6240af314a70814c4f3
MatSuzart <mathsuzart@outlook.com>
2025-10-28 19:46:15 -0300

    ✨service-order-create

--- src/app/api/order/new/route.ts
+++ src/app/api/order/new/route.ts
@@ -1,7 +1,13 @@
+import { getLoggedUser } from "@/services/auth";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request){
     const { cart } = await request.json();
+    const loggedUser = await getLoggedUser()
 
+    if(!loggedUser){ return NextResponse.json({error: 'User error loggin.'})};
+    if(!cart  || (cart.length === 0)) return NextResponse.json({error: 'Cart is empty.'});
+    
+    //console.log(cart);
     return NextResponse.json({});
 }
---
+++ src/services/order.ts
@@ -0,0 +1,5 @@
+import { CartItem } from "@/types/cart-item";
+
+export const createOrder = async (cart: CartItem[])=>{
+    
+}
